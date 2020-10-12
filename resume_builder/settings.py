704dd6ef081d3bc42e945db7420db74d51abbc53
gawema <gab.mannu@hotmail.it>
2020-10-12 10:42:25 +0200

    allowed hosts

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -27,9 +27,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'resume-builder-gwm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
